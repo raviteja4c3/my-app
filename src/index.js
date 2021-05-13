6adbb55002db65cd704bba2b6eadc86a8bb0d132
Ravi <raviteja4c3@gmail.com>
2021-05-14 00:30:25 +0530

    Installed React Semantic UI package
    Installed React Semantic UI package.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,32 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import 'semantic-ui-css/semantic.min.css';
+import { Button } from 'semantic-ui-react';
+
+const item = <p>This is an item element</p>
+const Item = (props) => {
+  return(
+    <h1>This is Item Component. My name is: {props.name} </h1>
+  )
+}
+const Item2 = () => {
+  return(
+    <h1>This is Item2 Component. </h1>
+  )
+}
+const element1 = <div>
+  <h1>Hello, Element3</h1>
+  {item}
+  {item}
+  <Item name="Ravi"/>
+  <Item2 />
+</div>
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    // <App />,
+    // Item name="Teja" />,
+     element1,
   document.getElementById('root')
 );
 

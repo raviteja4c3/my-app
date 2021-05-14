c6b021d3002fe4ecb0f5a7a7967be8911a20c6a1
Ravi <raviteja4c3@gmail.com>
2021-05-14 19:08:21 +0530

    Created App Class component
    Changed App function component to a Class component and added constructor method to store the state and used the state.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 import Myheader from './components/Myheader';
+import React from 'react';
 
+// function App() {
+//   return (
+//     <div className="App">
+//       <Myheader name="Ravi"/>
+//     </div>
+//   );
+// }
 
-function App() {
-  return (
-    <div className="App">
-      <Myheader name="Ravi"/>
-    </div>
-  );
+//Changed App function component to a App class component. 
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      name: "Teja's"
+    }
+  }
+
+  render(){
+    return(
+      <div className="App">
+        <Myheader name={this.state.name} author={'Ravi'} />
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/components/Myheader.js
+++ src/components/Myheader.js
@@ -3,11 +3,11 @@ import React from 'react';
 import {Segment, Header, Image} from 'semantic-ui-react';
 
 
-const Myheader = ({name}) => {
+const Myheader = ({name, author}) => {
     return(
         <Segment inverted vertical padded='very'>
             <Header as='h1'>This is header for {name} AutoVault!</Header>
-            <Header as='h2'>This is subheader</Header>
+            <Header as='h2'>This is subheader. Author is: {author}</Header>
             <Image src={'TejaLogo2.png'} size='small' centered/>
         </Segment>
     )
